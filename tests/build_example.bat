9222d292bcb4f079d2833df03dbf633ee5f089bd
Samantha Bartlett <sammynilla@protonmail.com>
2021-03-12 17:33:44 -0500

    just adding some comments to the build example

--- tests/build_example.bat
+++ tests/build_example.bat
@@ -1,10 +1,14 @@
 @ECHO OFF
 SETLOCAL
 ::
+
+### Our setup_cl_generic.bat happens to be in the directory above the test dir.
+### Optimally it should be added to PATH somehow for proper use, or bundled with projects (bin directory).
 PUSHD "../"
 CALL "setup_cl_generic.bat" x64
 POPD
 
+### YOUR MSVC COMPILATION PROCESS HERE
 CL
 ::
 ENDLOCAL
